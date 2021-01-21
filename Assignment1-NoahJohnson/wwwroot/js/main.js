01a317a25e39ddcccda25b099ae937e4739cb18b
Noah Johnson <jnoahjohnson@gmail.com>
2021-01-20 17:00:27 -0700

    Add comments

--- Assignment1-NoahJohnson/wwwroot/js/main.js
+++ Assignment1-NoahJohnson/wwwroot/js/main.js
@@ -1,17 +1,24 @@
-﻿submit = document.getElementById("grade-submit");
+﻿// Get the submit button
+submit = document.getElementById("grade-submit");
+
+// Add an event listener to the submit button
 
 submit.addEventListener("click", e => {
+    // Prevent the default submit action
     e.preventDefault();
-    console.log("submit");
-
+    
+    // Get all of the grades for each category
     let assignment = parseInt(document.getElementById("assignment").value);
     let project = parseInt(document.getElementById("project").value);
     let quiz = parseInt(document.getElementById("quiz").value);
     let exam = parseInt(document.getElementById("exam").value);
     let intex = parseInt(document.getElementById("intex").value);
 
+    // Calculate the final grade 
     let finalGrade = (assignment * .5) + (project * .1) + (quiz * .1) + (exam * .2) + (intex * .1);
 
+
+    // Calculate the letter grade based on the final grade 
     let letterGrade = "";
 
     if (finalGrade >= 94) {
@@ -40,10 +47,10 @@ submit.addEventListener("click", e => {
         letterGrade = "E";
     }
 
+    // Get the div where the final grade will be shown
     let finalGradeContainer = document.getElementById("final-grade");
 
+    // Set the html of the final grade div and then show the element
     finalGradeContainer.innerHTML = `<h3>Final Grade</h3><p id="letter-grade">${letterGrade}</p><p id="numeric-grade">${finalGrade}</p>`
     finalGradeContainer.hidden = false;
-   
-
 });
